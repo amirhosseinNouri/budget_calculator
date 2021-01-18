842d5e66cac339ccc620528d9c2842c75074f9ab
Amirhossein <amir.h.nouri2000@gmail.com>
2021-01-18 15:33:50 +0330

    Calculating total amount added

--- src/App.js
+++ src/App.js
@@ -1,15 +1,35 @@
+import { useState } from "react";
+import "./App.css";
+import Alert from "./components/Alert";
+import ExpenseList from "./components/ExpenseList";
+import ExpenseForm from "./components/ExpenseForm";
+import { v4 as uuidv4 } from "uuid";
 
-import './App.css';
-import Alert from './components/Alert'
-import ExpenseList from './components/ExpenseList'
-import ExpenseForm from './components/ExpenseForm'
+const initialExpenses = [
+  { id: uuidv4(), charge: "rent", amount: 1300 },
+  { id: uuidv4(), charge: "car payment", amount: 1500 },
+  { id: uuidv4(), charge: "credit card bill", amount: 200 },
+];
 
 function App() {
+  const [expenses, setExpenses] = useState(initialExpenses);
   return (
     <>
       <Alert></Alert>
-      <ExpenseForm></ExpenseForm>
-      <ExpenseList></ExpenseList>
+      <h1>budget calculator</h1>
+      <main className="app">
+        <ExpenseForm></ExpenseForm>
+        <ExpenseList expenses={expenses}></ExpenseList>
+      </main>
+      <h1>
+        total spending :{" "}
+        <span className="total">
+          $
+          {expenses.reduce((total, current) => {
+            return (total += current.amount);
+          }, 0)}
+        </span>
+      </h1>
     </>
   );
 }
