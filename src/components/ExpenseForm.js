302b28cee26e3306464cdf8efb2d5d75fa12e591
Amirhossein <amir.h.nouri2000@gmail.com>
2021-01-18 19:11:30 +0330

    ExpenseForm jsx added and styled

--- src/components/ExpenseForm.js
+++ src/components/ExpenseForm.js
@@ -1,10 +1,39 @@
-import React from 'react'
+import React from "react";
+import { MdSend } from "react-icons/md";
 
 export const ExpenseForm = () => {
-    return (
-        <div>
-            Expense Form
+  return (
+    <form>
+      <div className="form-center">
+        <div className="form-group">
+          <label htmlFor="charge">Charge</label>
+          <input
+            type="text"
+            className="form-control"
+            id="charge"
+            name="charge"
+            placeholder="e.g. Rent"
+          />
+
         </div>
-    )
-}
-export default ExpenseForm
+
+        <div className="form-group">
+          <label htmlFor="amount">Amount</label>
+          <input
+            type="text"
+            className="form-control"
+            id="amount"
+            name="amount"
+            placeholder="e.g. 100"
+          />
+
+        </div>
+      </div>
+      <button type="submit" className="btn">
+          Submit
+          <MdSend className="btn-icon"></MdSend>
+      </button>
+    </form>
+  );
+};
+export default ExpenseForm;
