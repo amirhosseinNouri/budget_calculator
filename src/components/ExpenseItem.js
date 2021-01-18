1128ba2d471d23317a965c658dd38427c3465662
Amirhossein <amir.h.nouri2000@gmail.com>
2021-01-18 17:00:34 +0330

    ExpenseItem component doen

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,11 +1,24 @@
-import React from 'react'
+import React from "react";
+import { MdEdit, MdDelete } from "react-icons/md";
 
-export const ExpenseItem = () => {
-    return (
-        <div>
-            Expense Item
-        </div>
-    )
-}
+export const ExpenseItem = ({ expense }) => {
+  const { id, charge, amount } = expense;
+  return (
+    <li className="item">
+      <div className="info">
+        <span className="expense">{charge}</span>
+        <span className="amount">${amount}</span>
+      </div>
+      <div>
+      <button className="edit-btn" aria-label="edit button">
+        <MdEdit></MdEdit>
+      </button>
+      <button className="clear-btn" aria-label="Delete button">
+        <MdDelete></MdDelete>
+      </button>
+      </div>
+    </li>
+  );
+};
 
-export default ExpenseItem
+export default ExpenseItem;
